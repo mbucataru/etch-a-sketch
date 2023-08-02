5d27ec9cef7a3d7ae1a410e3b532a6bf393535bd
mbucataru <mbucatarugithub@gmail.com>
2023-08-01 21:23:22 -0400

    Add grid resizing functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -18,3 +18,38 @@ function draw(e)
 
 tiles.forEach(tile => tile.addEventListener('mouseover', draw));
 
+const button = document.querySelector("button");
+
+function createNewGrid (e)
+{
+    let newSize = prompt("How many boxes per side?");
+    if (newSize <= 100)
+    {
+        while (container.firstChild)
+        {
+            container.removeChild(container.lastChild);
+        }
+        let proportion = 100 / newSize;
+
+        for (let i = 0; i < newSize * newSize; i++)
+        {
+            const newTile = document.createElement("div");
+            newTile.classList.add("tile");
+            newTile.style.width = `${proportion}%`;
+            newTile.style.height = `${proportion}%`;
+            container.appendChild(newTile);
+        }  
+
+        const newTiles = document.querySelectorAll(".tile");
+
+        newTiles.forEach(tile => tile.addEventListener('mouseover', draw));
+    }
+    
+    
+    
+    // tiles.forEach(tile => tile.style.backgroundColor = "grey");
+
+}
+
+button.addEventListener('click', createNewGrid);
+
